a11d52ee141d4c42724fc8f44379f2d76f36fb5a
m-ornella <marieornella.vitry@gmail.com>
2024-02-21 13:00:59 +0100

    refacto files

--- first.py
+++ first.py
@@ -2,22 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# product_page_url
-# universal_product_code [upc]
-# title
-# price_including_tax
-# price_excluding_tax
-# number_available
-# product_description
-# category
-# review_rating
-# image_url
-
 
 def get_book(sub_url):
 
     base_url = "https://books.toscrape.com/catalogue/"
-    category_slug = "/category"
     page = requests.get(base_url + sub_url + "/index.html")
 
     soup = BeautifulSoup(page.text, "html.parser")
@@ -57,36 +45,5 @@ def write_book_info_to_csv(book_info, filename="book.csv"):
             f.write(f"{key},{value}\n")
 
 
-# book_data = get_book("mesaerion-the-best-science-fiction-stories-1800-1849_983")
-# print(book_data)
-
-
-def get_books_urls_by_category(category_name, category_id) -> list[str]:
-    url = f"https://books.toscrape.com/catalogue/category/books/{category_name}_{category_id}/index.html"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-
-    book_urls = []
-
-    for book in soup.find_all("article", class_="product_pod"):
-        link = book.find("a")
-        book_url = link.get("href")
-        absolute_book_url = f"https://books.toscrape.com/catalogue/{book_url}"
-        book_urls.append(absolute_book_url)
-
-    return book_urls
-
-
-travel_book_urls = get_books_urls_by_category("travel", "2")
-print(travel_book_urls)
-
-
-# def get_books_by_category():
-#    get_book
-
-
-#     return fiction_books
-
-
-# fiction_books = get_books_by_category()
-# print(fiction_books)
+book_data = get_book("mesaerion-the-best-science-fiction-stories-1800-1849_983")
+print(book_data)
