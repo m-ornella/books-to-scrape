2d8ea654a5ffb36128310e03559b6ba2bda75987
m-ornella <marieornella.vitry@gmail.com>
2024-02-21 12:55:43 +0100

    looping through travel books url

--- main.py
+++ main.py
@@ -57,9 +57,36 @@ def write_book_info_to_csv(book_info, filename="book.csv"):
             f.write(f"{key},{value}\n")
 
 
-book_data = get_book("mesaerion-the-best-science-fiction-stories-1800-1849_983")
-print(book_data)
+# book_data = get_book("mesaerion-the-best-science-fiction-stories-1800-1849_983")
+# print(book_data)
 
 
-def get_books_by_category():
-    get_book(sub_url="/category/books/fiction_10")
+def get_books_urls_by_category(category_name, category_id) -> list[str]:
+    url = f"https://books.toscrape.com/catalogue/category/books/{category_name}_{category_id}/index.html"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    book_urls = []
+
+    for book in soup.find_all("article", class_="product_pod"):
+        link = book.find("a")
+        book_url = link.get("href")
+        absolute_book_url = f"https://books.toscrape.com/catalogue/{book_url}"
+        book_urls.append(absolute_book_url)
+
+    return book_urls
+
+
+travel_book_urls = get_books_urls_by_category("travel", "2")
+print(travel_book_urls)
+
+
+# def get_books_by_category():
+#    get_book
+
+
+#     return fiction_books
+
+
+# fiction_books = get_books_by_category()
+# print(fiction_books)
