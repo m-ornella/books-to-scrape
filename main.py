8dff01fb6cbe37544c6dadf40efbe9c841fbc2d7
m-ornella <marieornella.vitry@gmail.com>
2024-02-21 10:48:16 +0100

    refacto

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def get_book(sub_url):
     base_url = "https://books.toscrape.com/catalogue/"
     category_slug = "/category"
     page = requests.get(base_url + sub_url + "/index.html")
-    # page_category = requests
 
     soup = BeautifulSoup(page.text, "html.parser")
 
@@ -49,57 +48,18 @@ def get_book(sub_url):
         "image_url": image_url,
     }
 
-    with open("book.csv", "w") as f:
-        for key in book_info.keys():
-            f.write("%s,%s\n" % (key, book_info[key]))
-
     return book_info
+
+
+def write_book_info_to_csv(book_info, filename="book.csv"):
+    with open(filename, "w") as f:
+        for key, value in book_info.items():
+            f.write(f"{key},{value}\n")
 
 
 book_data = get_book("mesaerion-the-best-science-fiction-stories-1800-1849_983")
 print(book_data)
 
 
-# def get_books_by_category():
-#     page = requests.get(
-#         "https://books.toscrape.com/catalogue/category/books/fiction_10/index.htmll"
-#     )
-
-#     soup = BeautifulSoup(page.text, "html.parser")
-
-#     books_by_gategory = []
-#     books = soup.find_all(class_="product_pod")
-#     for book in books:
-#         title = soup.select_one("h1").text
-#         universal_product_code = soup.select_one("table tr:first-child td").text
-#         price_including_tax = soup.select_one("table tr:nth-child(3) td").text.replace(
-#             "Â£", ""
-#         )
-#         price_excluding_tax = soup.select_one("table tr:nth-child(4) td").text.replace(
-#             "Â£", ""
-#         )
-#         number_available = soup.select_one("table tr:nth-child(6) td").text
-#         product_description = soup.select_one(".product_page > p").text
-#         category = soup.select_one(".breadcrumb li:nth-child(3) a").text
-#         review_rating = soup.find("p", class_="star-rating")["class"][1]
-#         image_url = [img["src"] for img in soup.select(".carousel-inner img[src]")][0]
-#         print(book)
-#         books_by_gategory.append(
-#             {
-#                 "title": title,
-#                 "universal_product_code": universal_product_code,
-#                 "price_including_tax": price_including_tax,
-#                 "price_excluding_tax": price_excluding_tax,
-#                 "number_available": number_available,
-#                 "product_description": product_description,
-#                 "category": category,
-#                 "review_rating": review_rating,
-#                 "image_url": image_url,
-#             }
-#         )
-
-#         return books_by_gategory
-
-
-# fiction_books = get_books_by_category()
-# print(fiction_books)
+def get_books_by_category():
+    get_book(sub_url="/category/books/fiction_10")
