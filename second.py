c9d50656a8a6069fdfd3daef1e01c0f048c76045
m-ornella <marieornella.vitry@gmail.com>
2024-02-24 11:52:27 +0100

    next page navigation logic

--- second.py
+++ second.py
@@ -5,23 +5,33 @@ import csv
 from first import get_book
 
 
+def navigate_to_next_page(soup, category_name, category_id):
+    next_button = soup.find("li", class_="next")
+    if next_button and next_button.find("a"):
+        next_page_url = f"https://books.toscrape.com/catalogue/category/books/{category_name}_{category_id}/{next_button.find('a')['href']}"
+        return next_page_url
+    return None
+
+
 # loop through all book urls in travel category
 def get_books_urls_by_category(category_name, category_id) -> list[str]:
-    url = f"https://books.toscrape.com/catalogue/category/books/{category_name}_{category_id}/index.html"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
+    base_url = f"https://books.toscrape.com/catalogue/category/books/{category_name}_{category_id}/index.html"
+    current_page_url = base_url
 
     book_urls = []
 
-    for book in soup.find_all("article", class_="product_pod"):
-        link = book.find("a")
-        book_url = link.get("href")
-        absolute_book_url = f"https://books.toscrape.com/catalogue/{book_url[9:]}"
-        book_urls.append(absolute_book_url)
+    while current_page_url:
+        page = requests.get(current_page_url)
+        soup = BeautifulSoup(page.text, "html.parser")
 
-    # with open("book_category_urls.csv", "w") as f:
-    #     for url in book_urls:
-    #         f.write(f"{url}\n")
+        for book in soup.find_all("article", class_="product_pod"):
+            link = book.find("a")
+            book_url = link.get("href")
+            absolute_book_url = f"https://books.toscrape.com/catalogue/{book_url[9:]}"
+            book_urls.append(absolute_book_url)
+
+        # Navigate to the next page if available
+        current_page_url = navigate_to_next_page(soup, category_name, category_id)
 
     return book_urls
 
@@ -56,6 +66,6 @@ def write_books_to_csv(books, filename="books_by_category.csv"):
         writer.writerows(books)
 
 
-book_details = get_books_by_category("travel", "2")
+book_details = get_books_by_category("mystery", "3")
 print(book_details)
 write_books_to_csv(book_details)
